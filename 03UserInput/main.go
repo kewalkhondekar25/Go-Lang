e781b737d96542b9b48ceaf8d355ba67791ce19c
keal khondekar <kewalkhondekar25@gmail.com>
2023-10-05 00:41:17 +0530

    feat - bufio os pkg

--- 03UserInput/main.go
+++ 03UserInput/main.go
@@ -10,9 +10,11 @@ func main() {
 	welcome := "welcome to user input"
 	fmt.Println(welcome)
 
+	//NewReader return a new Reader && os reads from I/O
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter the rating for our consultation service: ")
 
+	// comma ok || err ok
 	input, _ := reader.ReadString('\n')
 	fmt.Println("Thanks for rating us", input)
 }
