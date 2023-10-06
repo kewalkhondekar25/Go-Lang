9322a186c80b3f59f05c9d621898c3f1d2baaa5f
keal khondekar <kewalkhondekar25@gmail.com>
2023-10-07 00:18:01 +0530

    feat - pointers added

--- 06MyPointer/main.go
+++ 06MyPointer/main.go
@@ -7,6 +7,10 @@ func main() {
 	fmt.Println("welcome to pointers")
 
 	var ptr *int
-
 	fmt.Println("value of pointer :", ptr)
+
+	myNumber := 23
+	myPointer := &myNumber
+	fmt.Println("value of actual pointer is :", myPointer)
+	fmt.Println("value of actual pointer is :", *myPointer)
 }
